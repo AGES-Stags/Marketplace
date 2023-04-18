69ca23bb8f856e4ff1c620e0432a85dee01c3545
hcley <cleysonb.oliveira@gmail.com>
2023-04-18 15:31:43 -0300

    Added LinkedList style to ProductList class

--- src/ProductList.java
+++ src/ProductList.java
@@ -1,33 +1,68 @@
-import java.util.Arrays;
 
 public class ProductList {
 	private static ProductList list;
-	private Product[] productList;
-	private int i=0;
+
+	private Node head;
+	private Node tail;
+	private int size;
 	
-	private ProductList(){
-		productList = new Product[3];
+
+	private class Node {
+		Product product;
+		Node next;
+		Node previous;
+
+		public Node(Product product) {
+			this.product = product;
+		}
+
+	}
+
+	private ProductList() {
+		head = tail = null;
+		size = 0;
 	}
 
 	public static ProductList getInstance(){
 		if(list == null) list = new ProductList();
 		return list;
-	} 
-
-	public void addProduct(Product product){
-		if(productList.length <= i ){
-			Product[] array = new Product[i*2];
-			for(int i=0; i<productList.length;i++){
-				array[i] = productList[i];
-			}
-			productList = array;
-		}
-		productList[i] = product;
-		++i;
 	}
+
+	public void add(Product product) {
+		Node node = new Node(product);
+
+		if(head == null) {
+			head = tail = node;
+			size++;
+			return;
+		}
+
+		tail.next = node;
+		node.previous = tail;
+		tail = node;
+		size++;
+	}
+
+	public Product getIndex(int index) {
+		Node node = head;
+
+		for(int i = 0; i < index; i ++) {
+			node = node.next;
+		}
+
+		return node.product;
+	}
+
+
+	public Product getIndexReverse(int index) {
+		Node node = head;
+			// TODO
+		return node.product;
+	}
+
 
 	@Override
 	public String toString(){
-		return Arrays.toString(productList);
+		return "";
 	}
 }
