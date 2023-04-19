7b4b8990c96a6a881b859514b7dd836f78c8b742
Cleyson Braga de Oliveira <Cleysonb.Oliveira@gmail.com>
2023-04-18 23:52:39 -0300

    Prepared run script to be able to import external custom libraries

--- run.sh
+++ run.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-javac ./src/*.java -d ./bin
+# Validate if a path was given by parameters
+# If not, set currentDirectory with the terminals pwd path
+if [ $# -eq 0 ]; then
+	currentDirectory="$(pwd)"
+elif [ ! -z "$1" ]; then
+    currentDirectory=$1
+fi
 
+# Create java run classpath execution parameter
+if [ -d "$currentDirectory/lib/" ]; then
+	libs="-cp $currentDirectory/lib/DevTools.jar"
+	classpath="-cp .:$currentDirectory/lib/DevTools.jar:$currentDirectory/bin"
+else
+	classpath="-cp $currentDirectory/bin"
+fi;
+
+# Compile java whether or not having libraries
+javac $libs $currentDirectory/src/*.java -d $currentDirectory/bin
+
+# If the compilation stage does not gave any error
+# Execute java program with given parameters
 if [ $? -eq 0 ]; then
-	java -cp ./bin App
+	java $classpath App
 fi;
--- src/Market.java
+++ src/Market.java
@@ -1,15 +1,14 @@
+
+import inject.*;
+
 public class Market {
 
-	public static void run(){
-		Product product = new Product("Geladeira",1000,"2 Portas");
-		Product product2 = new Product("Sofá", 670, "3 Lugares");
-		Product product3 = new Product("Mesa", 780, "6 Lugares");
-		Product product4 = new Product("Tábua", 7, "Madeira");
+	public static void run() {
 		ProductList productList = ProductList.getInstance();
-		
-		productList.add(product);
-		productList.add(product2);
-		productList.add(product3);
-		productList.add(product4);
-	}   
+		productList
+				.add(new Product("Kit pressostato", 119.98, "Pressostato, manometro e válvula de segurança 125 PSI"));
+
+		Mockup.sout();
+
+	}
 }
